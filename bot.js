84904616bf8bcd9fb1dd4dfc0390790db7610f4e
ohmyladykk <kenshi310310@gmail.com>
2026-03-25 11:20:12 +0900

    GitHub叩き起こし用の空撃ち！

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 async function sendLine() {
     // 宛先を指定して送信する「プッシュメッセージ」エンドポイント
     const url = "https://api.line.me/v2/bot/message/push";
-    
+
     // 環境変数からチャネルアクセストークンとユーザーIDを取得
     const token = process.env.LINE_CHANNEL_ACCESS_TOKEN;
     const userId = process.env.LINE_USER_ID;
@@ -24,7 +24,7 @@ async function sendLine() {
     // 日本時間にあわせて成形
     const timeString = now.toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" });
     const msg = `【勤怠連絡】\nおはようございます。本日は在宅勤務(テレワーク)にて業務を開始いたします。\n現在時刻: ${timeString}\nよろしくお願いいたします。`;
-    
+
     // LINE Messaging API 用のJSONフォーマット（"to" でユーザーIDを指定）
     const body = JSON.stringify({
         to: userId,
@@ -55,3 +55,4 @@ async function sendLine() {
 }
 
 sendLine();
+
